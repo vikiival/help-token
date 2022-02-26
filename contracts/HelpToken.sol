50cae555be283da36dfd2e2a47aeadf7b4897114
Viki Val <viktorko99@gmail.com>
2022-02-26 09:35:50 +0100

    :tada: Initial commit

---
+++ contracts/HelpToken.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract HelpToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint16 private _totalSupply = 10000; // 10,000 tokens
+    uint public tokenValue = 1e16 wei; // 0.01 ether
+
+    constructor(address owner_) ERC721("MyToken", "MTK") {
+        _transferOwnership(owner_);
+    }
+
+    function create(uint16 count) public payable {
+        require(count <= _totalSupply - _tokenIdCounter.current(), "Not enough tokens");
+        require(msg.value >= tokenValue * count, "Wrong token value");
+
+        for (uint16 i = 0; i < count; i++) {
+            safeMint(msg.sender);
+        }
+        
+        address payable destination = payable(owner());
+        destination.transfer(msg.value);
+    }
+
+    function totalSupply() public view returns (uint16) {
+        return _totalSupply;
+    }
+
+    function safeMint(address to) private {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
