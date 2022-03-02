7331dd60579dd79070fe36eb0586b039cb161823
Viki Val <viktorko99@gmail.com>
2022-03-02 23:07:04 +0100

    Update BatchHelp.sol

--- contracts/BatchHelp.sol
+++ contracts/BatchHelp.sol
@@ -18,7 +18,7 @@ contract HelpToken is ERC1155, Ownable {
         _symbol = symbol_;
     }
 
-    function create(uint16 count) public payable {
+    function create(uint16 count) external payable {
         require(count  <= _totalSupply - alreadyMinted, "Not enough fee tokens");
         require(msg.value >= tokenValue * count, "Not enough ETH sent");
         mint(msg.sender, 0, count);
