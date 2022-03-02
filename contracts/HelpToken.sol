9b51bea77622a06dbbf921956e59d21099c2727c
Viki Val <viktorko99@gmail.com>
2022-03-02 15:32:07 +0100

    Update HelpToken.sol

--- contracts/HelpToken.sol
+++ contracts/HelpToken.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract HelpToken is ERC721, Ownable {
+contract HelpToken is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -16,9 +17,13 @@ contract HelpToken is ERC721, Ownable {
         _transferOwnership(owner_);
     }
 
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmZjuWFrnf6upnQnhRdM5uYCDUfPu6nt9ehHuVaMdBtujf";
+    }
+
     function create(uint16 count) public payable {
-        require(count <= _totalSupply - _tokenIdCounter.current(), "Not enough free tokens");
-        require(msg.value >= tokenValue * count, "Not enough ETH to mint token(s)");
+        require(count <= _totalSupply - _tokenIdCounter.current(), "Not enough fee tokens");
+        require(msg.value >= tokenValue * count, "Wrong token value");
 
         for (uint16 i = 0; i < count; i++) {
             safeMint(msg.sender);
@@ -36,5 +41,20 @@ contract HelpToken is ERC721, Ownable {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+    }
+
+        // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return _baseURI();
     }
 }
